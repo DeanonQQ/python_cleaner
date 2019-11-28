cf53c6f468af66a6933a53d1e8d393472fe7247c
Denis <rodionovs45qwe@yandex.ru>
2019-11-29 00:15:41 +0400

    update

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import os
+import shutil
+import datetime
+import pathlib
 
+# ------------------------CONFIG-------------------------------
 
 CLEAN_PATH = "C:/Users/rodio/Desktop"
 CLEAN_FORMATS = ('.jpg', '.psd', '.jpeg', '.ai')
 FOLDER_NAME = "WorkFiles"
 
+# -------------------------------------------------------------
 
 
+now = datetime.datetime.now()
+
+MV_FOLDER = CLEAN_PATH + "/" + FOLDER_NAME + ' ' + str(now.strftime("%d-%m-%Y"))
+
+names = os.listdir(CLEAN_PATH)
+for name in names:
+    fullname = os.path.join(CLEAN_PATH, name)
+    if os.path.isfile(fullname):
+        if fullname.lower().endswith(('.ai', '.jpg', '.jpeg', 'psd', '.rar')):
+            if not os.path.exists(MV_FOLDER):
+                os.makedirs(MV_FOLDER)
+            
+            shutil.move(fullname, MV_FOLDER+'/'+name)
 
 
+            
+            
+            print(fullname)
